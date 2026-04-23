4d5bd1e67590be64a02e9a0152407f665eb38045
ThatThing02102009 <thatthing00001@gmail.com>
2026-04-23 20:19:39 +0700

    Build Fix: Docker extraction & Kernel sync - 2026-04-23

--- sync_and_push.sh
+++ sync_and_push.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m "Build Fix: Docker extraction & Kernel sync - $(date +'%Y-%m-%d')"
+git commit -m "Build Fix: Docker extraction & Kernel sync - $(date +'%Y-%m-%d')" || true
+git pull --rebase origin main
 git push origin main
